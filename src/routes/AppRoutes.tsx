9e4426b6cda42e15848bb403f0a26891d49fff4d
Om Deshmukh <omdeshmukh1906@gmail.com>
2026-05-15 13:00:06 +0530

    fix dashboard typescript build errors

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,27 +3,20 @@ import DashboardLayout from '../components/DashboardLayout';
 import ProtectedRoute from './ProtectedRoute';
 import { appRoutes } from './routeConfig';
 import Login from '../pages/Login';
-import { ROLE_HOME_PATHS } from '../utils/roles';
-import { useAppSelector } from '../redux/hooks';
-
-function RoleRedirect() {
-  const { user } = useAppSelector(state => state.auth);
-  return <Navigate to={user ? ROLE_HOME_PATHS[user.role] : '/login'} replace />;
-}
 
 export default function AppRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route element={<ProtectedRoute />}>
         <Route element={<DashboardLayout />}>
-          <Route index element={<RoleRedirect />} />
           {appRoutes.map(route => (
             <Route key={route.path} path={route.path.slice(1)} element={route.element} />
           ))}
         </Route>
       </Route>
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
